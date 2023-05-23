57216fe9ffbc053bf05d5c371eb8e1bfc20053bc
unker <kfi.unknown@gmail.com>
2023-05-23 23:13:33 +0300

    lint ok

--- advanced/src/js/app.js
+++ advanced/src/js/app.js
@@ -1,7 +1,9 @@
 import orderByProps from './tablets/order';
 
 /* eslint-disable no-console */
-const obj = { name: 'мечник', health: 10, level: 2, attack: 80, defence: 40 };
+const obj = {
+  name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
+};
 const order = ['name', 'level', 'health'];
 
 const result = orderByProps(obj, order);
--- advanced/src/js/tablets/__tests__/order.test.js
+++ advanced/src/js/tablets/__tests__/order.test.js
@@ -1,42 +1,44 @@
-import orderByProps from "../order";
+import orderByProps from '../order';
 
 describe('orderByProps', () => {
-    let obj = { name: 'мечник', health: 10, level: 2, defence: 40, attack: 80 };
+  const obj = {
+    name: 'мечник', health: 10, level: 2, defence: 40, attack: 80,
+  };
 
-    it('should return an array of objects with properties sorted according to the order', () => {
-      const order = ['name', 'level', 'health'];
-  
-      const result = orderByProps(obj, order);
-  
-      expect(result).toEqual([
-        { key: 'name', value: 'мечник' },
-        { key: 'level', value: 2 },
-        { key: 'health', value: 10 },
-        { key: 'attack', value: 80 },
-        { key: 'defence', value: 40 }
-      ]);
-    });
-  
-    it('should return an array of objects with properties sorted in alphabetical order if not specified in the order array', () => {
-      const order = [];
-  
-      const result = orderByProps(obj, order);
-  
-      expect(result).toEqual([
-        { key: 'attack', value: 80 },
-        { key: 'defence', value: 40 },
-        { key: 'health', value: 10 },
-        { key: 'level', value: 2 },
-        { key: 'name', value: 'мечник' }
-      ]);
-    });
-  
-    it('should handle an empty object and empty order array', () => {
-      const obj = {};
-      const order = [];
-  
-      const result = orderByProps(obj, order);
-  
-      expect(result).toEqual([]);
-    });
+  it('should return an array of objects with properties sorted according to the order', () => {
+    const order = ['name', 'level', 'health'];
+
+    const result = orderByProps(obj, order);
+
+    expect(result).toEqual([
+      { key: 'name', value: 'мечник' },
+      { key: 'level', value: 2 },
+      { key: 'health', value: 10 },
+      { key: 'attack', value: 80 },
+      { key: 'defence', value: 40 },
+    ]);
   });
+
+  it('should return an array of objects with properties sorted in alphabetical order if not specified in the order array', () => {
+    const order = [];
+
+    const result = orderByProps(obj, order);
+
+    expect(result).toEqual([
+      { key: 'attack', value: 80 },
+      { key: 'defence', value: 40 },
+      { key: 'health', value: 10 },
+      { key: 'level', value: 2 },
+      { key: 'name', value: 'мечник' },
+    ]);
+  });
+
+  it('should handle an empty object and empty order array', () => {
+    const objNull = {};
+    const order = [];
+
+    const result = orderByProps(objNull, order);
+
+    expect(result).toEqual([]);
+  });
+});
