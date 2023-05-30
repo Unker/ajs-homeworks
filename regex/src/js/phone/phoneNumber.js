0bf58624c7b1d667eea04054d5b0e1b31d246330
unker <kfi.unknown@gmail.com>
2023-05-30 23:02:34 +0300

    lint done

--- regex/src/js/phone/phoneNumber.js
+++ regex/src/js/phone/phoneNumber.js
@@ -1,27 +1,25 @@
 export default function normalizePhoneNumber(phoneNumber) {
   // Проверяем на недопустимые символы ( +,-,),(,цифра,пробел )
-  const notValidSybolsRegex = /[^\d\s\(\)\+\-]/;
+  const notValidSybolsRegex = /[^\d\s()+-]/;
   if (notValidSybolsRegex.test(phoneNumber)) {
     return null;
   }
-  
+
   // удаляем пробелы и дефисы
   const stripPhone = phoneNumber.replace(/[\s-]/g, '');
 
   // Если первая 8, то заменим ее на +7
   const correctRussianStyle = stripPhone.replace(/^[8]/g, '+7');
-  
+
   // Проверяем корректность Международного стандарта (т.е. через "+")
-  // 1. вначале "+", потом чило, 
-  // 2. потом символ "(" (может не быть)
-  // 3. число
-  // 4. потом символ ")" (может не быть). Если есть, то должен быть "("
-  // 5. заканчиваться числом
+  // 1. вначале "+", потом чило,
+  // 2. потом число в круглых скобках "()" (может не быть)
+  // 3. заканчиваться числом
   const isPhoneNamberRegex = /^\+\d+(\(\d+\))?(\d+)$/;
   if (!isPhoneNamberRegex.test(correctRussianStyle)) {
     return null;
   }
-  
+
   // Удаление всех символов, кроме цифр
   const digitsOnly = correctRussianStyle.replace(/[\D+]/g, '');
 
@@ -30,6 +28,6 @@ export default function normalizePhoneNumber(phoneNumber) {
   if (digitsOnly.length > 15 || digitsOnly.length < 4) {
     return null;
   }
-  
+
   return `+${digitsOnly}`;
 }
