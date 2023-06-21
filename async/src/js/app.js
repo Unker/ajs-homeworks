0d0ad99e387e9e6a765662acd1cd3b73e5813790
unker <kfi.unknown@gmail.com>
2023-06-21 23:56:50 +0300

    used async/await

--- async/src/js/app.js
+++ async/src/js/app.js
@@ -1,8 +1,19 @@
 import GameSavingLoader from './gameSaver/gameSavingLoader';
 
+// цепочки .then().then()
 GameSavingLoader.load().then((saving) => {
   // saving объект класса GameSaving
   console.log(saving);
 }, (error) => {
   console.error(error);
 });
+
+// с использованием async/await
+(async () => {
+  try {
+    const gameSaving = await GameSavingLoader.load();
+    console.log(gameSaving);
+  } catch (error) {
+    console.error(error);
+  }
+})();
