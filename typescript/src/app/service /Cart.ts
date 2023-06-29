8a8dd2dc6aae121d93df7555bf282892df2477ab
unker <kfi.unknown@gmail.com>
2023-06-30 00:03:55 +0300

    new methods added

--- typescript/src/app/service /Cart.ts
+++ typescript/src/app/service /Cart.ts
@@ -10,4 +10,22 @@ export default class Cart {
   get items(): Buyable[] {
     return [...this._items];
   }
+
+  remove(id: number): void {
+    const index = this._items.findIndex(item => item.id === id);
+    if (index !== -1) {
+      this._items.splice(index, 1);
+    }
+  }
+
+  getTotalCost(): number {
+    return this._items.reduce((total, item) => total + item.price, 0);
+  }
+
+  getTotalCostWithDiscount(discount: number): number {
+    if (discount <= 0 || discount >= 1) {
+      throw new Error('Discount should be greater than 0 and less than 1.');
+    }
+    return this.getTotalCost() * (1 - discount);
+  }
 }
--- typescript/src/app/service /__tests__/Cart.test.ts
+++ typescript/src/app/service /__tests__/Cart.test.ts
@@ -47,4 +47,62 @@ describe('Cart', () => {
 
     expect(cart.items).toHaveLength(2);
   });
+
+  describe('remove', () => {
+    test('should remove an item from the cart', () => {
+      const item1 = { id: 10, name: 'Item 1', price: 10 };
+      cart.add(item1);
+      const item2 = { id: 20, name: 'Item 1', price: 10 };
+      cart.add(item2);
+      cart.remove(item1.id);
+      expect(cart.items).not.toContain(item1);
+    });
+
+    test('should not remove any item if the provided id does not exist', () => {
+      const item = { id: 1, name: 'Item 1', price: 10 };
+      cart.add(item);
+      cart.remove(999);
+      expect(cart.items).toContain(item);
+    });
+  });
+
+  describe('getTotalCost', () => {
+    test('should return 0 for an empty cart', () => {
+      expect(cart.getTotalCost()).toBe(0);
+    });
+
+    test('should return the total cost of items in the cart', () => {
+      const item1 = { id: 1, name: 'Item 1', price: 10 };
+      const item2 = { id: 2, name: 'Item 2', price: 20 };
+      const item3 = { id: 3, name: 'Item 3', price: 30 };
+      cart.add(item1);
+      cart.add(item2);
+      cart.add(item3);
+      expect(cart.getTotalCost()).toBe(60);
+    });
+  });
+
+  describe('getTotalCostWithDiscount', () => {
+    test('should throw an error if discount is less than or equal to 0', () => {
+      expect(() => cart.getTotalCostWithDiscount(0)).toThrow(
+        'Discount should be greater than 0 and less than 1.'
+      );
+    });
+
+    test('should throw an error if discount is greater than or equal to 1', () => {
+      expect(() => cart.getTotalCostWithDiscount(1)).toThrow(
+        'Discount should be greater than 0 and less than 1.'
+      );
+    });
+
+    test('should return the total cost with discount applied', () => {
+      const item1 = { id: 1, name: 'Item 1', price: 10 };
+      const item2 = { id: 2, name: 'Item 2', price: 20 };
+      const item3 = { id: 3, name: 'Item 3', price: 30 };
+      cart.add(item1);
+      cart.add(item2);
+      cart.add(item3);
+      expect(cart.getTotalCostWithDiscount(0.2)).toBe(48);
+    });
+  });
 });
